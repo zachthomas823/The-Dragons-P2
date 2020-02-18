d564826a4bc60f4c086be1e77003a344b3d3b7b1
zachthomas823 <zachthomas823@gmail.com>
2020-02-17 18:14:43 -0600

    refactored terraform to separate master and workers

--- terraform/master/master.tf
+++ terraform/master/master.tf
@@ -1,36 +1,9 @@
 provider "aws" {
   #Two localfiles names as such. Each contains what they say, given to you from AWS.
   #DO NOT UPLOAD THESE FILES, make sure they are masked by the .gitignore
-  access_key = "${file("./access_key")}"
-  secret_key = "${file("./secret_key")}"
+  access_key = "${file("../access_key")}"
+  secret_key = "${file("../secret_key")}"
   region     = "us-east-2"
-}
-
-resource "aws_instance" "worker" {
-  ami           = "ami-0fc20dd1da406780b"
-  instance_type = "t2.micro"
-
-  #Generate your own Key_Name from AWS and use that here
-  #DO NOT UPLOAD THESE FILES, make sure they are masked by the .gitignore
-  key_name = "Temp"
-  security_groups = ["${aws_security_group.SSH.name}"]
-
-  connection {
-    user = "ubuntu"
-    type = "ssh"
-    private_key = "${file("./Temp.pem")}"
-    host =  self.public_ip
-    timeout = "4m"
-  }
-  provisioner "file" {
-    source      = "setup_worker.sh"
-    destination = "/tmp/setup_worker.sh"
-  }
-  provisioner "remote-exec" {
-    inline = [
-      "sudo /bin/bash /tmp/setup_worker.sh",
-    ]
-  }
 }
 
 resource "aws_instance" "master" {
@@ -45,7 +18,7 @@ resource "aws_instance" "master" {
   connection {
     user = "ubuntu"
     type = "ssh"
-    private_key = "${file("./Temp.pem")}"
+    private_key = "${file("../Temp.pem")}"
     host =  self.public_ip
     timeout = "4m"
   }
@@ -62,12 +35,32 @@ resource "aws_instance" "master" {
     destination = "/tmp/setup_master.sh"
   }
   provisioner "file" {
-    source      = "../kubernetes/services/client-server.yaml"
+    source      = "../../kubernetes/services/client-server.yaml"
     destination = "/home/ubuntu/services/client-server.yaml"
   }
   provisioner "file" {
-    source      = "../kubernetes/pods/html-server.yaml"
+    source      = "../../kubernetes/pods/html-server.yaml"
     destination = "/home/ubuntu/pods/html-server.yaml"
+  }
+  provisioner "file" {
+    source      = "../worker/worker.tf"
+    destination = "/home/ubuntu/terraform/worker.tf"
+  }
+  provisioner "file" {
+    source      = "master.tf"
+    destination = "/home/ubuntu/terraform/master.tf"
+  }
+  provisioner "file" {
+    source      = "../secret_key"
+    destination = "/home/ubuntu/terraform/secret_key"
+  }
+  provisioner "file" {
+    source      = "../access_key"
+    destination = "/home/ubuntu/terraform/access_key"
+  }
+  provisioner "file" {
+    source      = "../Temp.pem"
+    destination = "/home/ubuntu/terraform/Temp.pem"
   }
 
   provisioner "remote-exec" {
@@ -77,8 +70,7 @@ resource "aws_instance" "master" {
   }
 }
 
-resource "aws_security_group" "SSH" {
-  name        = "allow_ssh"
+  resource "aws_security_group" "SSH" {
   description = "Allow SSH traffic"
 
 
