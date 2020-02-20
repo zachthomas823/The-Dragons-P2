b6f55dbb9ba7b990eaa3c5cd4e89c2f20ffec965
zachthomas823 <zachthomas823@gmail.com>
2020-02-20 00:26:23 -0600

    added rules for aws autoscaling group

---
+++ terraform/master/master_base/setup_master_base.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#Remove Bad Docker Installs
+apt-get remove docker docker-engine docker.io containerd runc
+
+apt-get update
+
+#Make sure everything required is installed
+apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+#grab key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+
+#Add Download location
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get update
+
+#Install Docker Proper
+apt-get -y install docker-ce docker-ce-cli containerd.io
+
+#Kuber requirements
+sed -i '/swap/d' /etc/fstab
+swapoff -a
+
+#Get Kubelet kubeadm kubectl
+apt-get update && apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
