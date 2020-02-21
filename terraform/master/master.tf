433e855477632e6a0fc9895beed56968295c9f2d
zachthomas823 <zachthomas823@gmail.com>
2020-02-21 02:17:57 -0600

    full auto start up

--- terraform/master/master.tf
+++ terraform/master/master.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   #Two localfiles names as such. Each contains what they say, given to you from AWS.
   #DO NOT UPLOAD THESE FILES, make sure they are masked by the .gitignore
-  # access_key = file("../access_key")
-  # secret_key = file("../secret_key")
+  access_key = file("../access_key")
+  secret_key = file("../secret_key")
   region     = "us-east-2"
 }
 
@@ -90,15 +90,21 @@ resource "aws_instance" "master" {
       "cd worker/worker_as",
       "sudo chmod 777 join.sh",
       "kubeadm token create --print-join-command >> join.sh",
-      #". /home/ubuntu/.bashrc",
-      "exec bash",
       "terraform init",
       "terraform apply --auto-approve",
       "cd ../../../sdn",
       "chmod 777 run.sh",
-      "./run.sh"
+      # "./run.sh"
     ]
   }
+
+  # provisioner "local-exec" {
+  #   command= "ssh -i '../Temp.pem' ubuntu@${self.public_ip};yes;cd sdn;./run.sh "
+  # }
+}
+
+output "instance_ip" {
+  value=aws_instance.master.public_ip
 }
 
   resource "aws_security_group" "SSH" {
@@ -121,10 +127,10 @@ resource "aws_instance" "master" {
   }
 }
 
-terraform {
-  backend "s3" {
-      bucket = "dragons-master-p2"
-      key    = "terraform.tfstate"
-      region = "us-east-2"
-  }
-}
+# terraform {
+#   backend "s3" {
+#       bucket = "the-dragons-master"
+#       key    = "terraform.tfstate"
+#       region = "us-east-2"
+#   }
+# }
